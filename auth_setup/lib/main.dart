c10b329440003e55802395a5eb0fd28230edc96b
prakaship78 <prakaship78@gmail.com>
2020-10-26 18:44:53 +0530

    User sign in and signout

---
+++ auth_setup/lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter demo app',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+
+  Future<User> signIn() async {
+    GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+    GoogleSignInAuthentication gSA = await googleSignInAccount.authentication;
+    final AuthCredential authCredential = GoogleAuthProvider.credential(
+        idToken: gSA.idToken, accessToken: gSA.accessToken);
+    final UserCredential userCredential =
+        await _auth.signInWithCredential(authCredential);
+    User user = userCredential.user;
+    print(user.displayName);
+    return user;
+  }
+
+  signOut() async {
+    googleSignIn.signOut();
+    print('user signed out');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Firebase Demo'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            RaisedButton(
+              onPressed: () => signIn()
+                  .then((User user) => print(user))
+                  .catchError((e) => print(e)),
+              child: Text('Sign In'),
+              color: Colors.green,
+            ),
+            RaisedButton(
+              onPressed: () {
+                signOut();
+              },
+              child: Text('Sign Out'),
+              color: Colors.red,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
