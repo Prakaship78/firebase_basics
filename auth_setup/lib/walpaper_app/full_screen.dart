4ca58f85f9b3ba539e6f4c2ebf3fe3a229655cbf
prakaship78 <prakaship78@gmail.com>
2020-11-01 12:52:57 +0530

    add set as wallpaper button

--- auth_setup/lib/walpaper_app/full_screen.dart
+++ auth_setup/lib/walpaper_app/full_screen.dart
@@ -2,8 +2,11 @@ import 'package:dio/dio.dart';
 import 'package:ext_storage/ext_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_downloader/flutter_downloader.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:wallpaperplugin/wallpaperplugin.dart';
 
 // ignore: must_be_immutable
 class FullScreen extends StatefulWidget {
@@ -16,8 +19,10 @@ class FullScreen extends StatefulWidget {
 
 class _FullScreen extends State<FullScreen> {
   bool downloading = false;
+  var downloadtask;
 
   var progressString = " ";
+  var filePath = '';
 
   final LinearGradient backgroundGradient = LinearGradient(
       colors: [Color(0x10000000), Color(0x30000000)],
@@ -32,6 +37,8 @@ class _FullScreen extends State<FullScreen> {
     var imageName = widget.imgPath.split('/').last;
     imageName = imageName.split('?').first;
     print('imagename: $imageName');
+
+    print('path: $dir/$imageName');
     if (status.isGranted) {
       final id = await FlutterDownloader.enqueue(
           url: widget.imgPath,
@@ -39,9 +46,61 @@ class _FullScreen extends State<FullScreen> {
           fileName: '$imageName',
           showNotification: true,
           openFileFromNotification: true);
+      downloadtask = 'Done';
     } else {
       print('Permission denied');
     }
+    return downloadtask;
+  }
+
+  Future<void> setWallpaper() async {
+    downloading = true;
+    final status = await Permission.storage.request();
+    var dir = await ExtStorage.getExternalStoragePublicDirectory(
+        ExtStorage.DIRECTORY_DOWNLOADS);
+    print('dir:$dir');
+    var imageName = widget.imgPath.split('/').last;
+    imageName = imageName.split('?').first;
+    print('imagename: $imageName');
+
+    print('path: $dir/$imageName');
+    if (status.isGranted) {
+      final id = await FlutterDownloader.enqueue(
+          url: widget.imgPath,
+          savedDir: dir,
+          fileName: '$imageName',
+          showNotification: false,
+          openFileFromNotification: false);
+      progressString = 'Completed';
+
+      setState(() {
+        filePath = '$dir/$imageName';
+        downloading = false;
+      });
+      Wallpaperplugin.setAutoWallpaper(localFile: filePath);
+      Navigator.pop(context);
+    } else {
+      print('Permission denied');
+    }
+    // Navigator.pop(context);
+  }
+
+  ontapProgress(context, values) {
+    return AlertDialog(
+      title: Text('Set as Wallpaper'),
+      actions: [
+        FlatButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text('No')),
+        FlatButton(
+            onPressed: () {
+              setWallpaper();
+            },
+            child: Text('Yes')),
+      ],
+    );
   }
 
   @override
@@ -103,7 +162,13 @@ class _FullScreen extends State<FullScreen> {
                       ),
                     ),
                     RaisedButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        showDialog(
+                          context: context,
+                          builder: (context) =>
+                              ontapProgress(context, widget.imgPath),
+                        );
+                      },
                       child: Text(
                         'walpaper',
                         style: TextStyle(color: Colors.black),
